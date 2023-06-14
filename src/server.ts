58c18f16fdbc9f4bcb9a1e1d77dd1de62a8b2946
alexandrefb <alexandrefb.86@gmail.com>
2023-06-14 07:36:10 -0300

    Configurações express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-const teste = "dsadas";
+import express from "express";
+
+const port = 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Home page");
+});
+
+app.listen(port, () => {
+    console.log(`Servidor em execucao na porta ${port}`);
+    
+});
