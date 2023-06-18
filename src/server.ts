82290f15545754d67a796284108cd56ad0a334e8
alexandrefb <alexandrefb.86@gmail.com>
2023-06-17 21:22:43 -0300

    Add rota de lista de movies

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const port = 3000;
 const app = express();
+const prisma = new PrismaClient();
 
-app.get("/movies", (req, res) => {
-    res.send("Listagem de filmes");
+app.get("/movies",async (req, res) => {
+    //busca os filmes do model movies
+    const movies = await prisma.movie.findMany();
+    //retorna os dados como json
+    res.json(movies);
 });
 
 app.listen(port, () => {
