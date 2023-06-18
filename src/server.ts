9f93c7a36cad364f37c7c7cda1429f62e56592cc
alexandrefb <alexandrefb.86@gmail.com>
2023-06-18 10:44:23 -0300

    Adicionada atualização de filme

--- src/server.ts
+++ src/server.ts
@@ -62,6 +62,44 @@ app.post("/movies", async (req, res) => {
     res.status(201).send();
 });
 
+app.put("/movies/:id", async (req, res) => {
+    //pegar o id do registro que vai ser atualizado
+    const id = Number(req.params.id); 
+
+    try {
+        //Valida se existe o filme registrado e retorna mensagem caso nao tenha
+        const movie = await prisma.movie.findUnique({
+            where: {
+                id: id
+            }
+        });
+
+        if(!movie) {
+            return res.status(404).send({
+                message: "Filme não encontrado"
+            });
+        }
+
+        const data = { ...req.body };
+        //Valida o release_data pq tem que ser no formato data
+        data.release_date = data.release_date ? new Date(data.release_date) : undefined;
+
+        //pegar os dados que quero atualizar e atualizar ele no prisma
+        await prisma.movie.update({
+            where: {
+                id: id
+            },
+            data: data
+        });
+
+    } catch (error) {
+        return res.status(500).send({ message: "Falha ao atualizar o registro do filme." });
+    }
+
+    //retornar o status correto informando que o filme foi atualizado
+    res.status(200).send({ message: "Atualizado com sucesso" });
+});
+
 app.listen(port, () => {
     console.log(`Servidor em execucao na porta ${port}`);
 });
