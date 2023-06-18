c286bcc1f49b1b7b8c25aa7304144562f3c8a9d3
alexandrefb <alexandrefb.86@gmail.com>
2023-06-18 11:20:01 -0300

    Adicionada Remoção de filmes

--- src/server.ts
+++ src/server.ts
@@ -100,6 +100,30 @@ app.put("/movies/:id", async (req, res) => {
     res.status(200).send({ message: "Atualizado com sucesso" });
 });
 
+app.delete("/movies/:id", async (req, res) => {
+    const id = Number(req.params.id);
+
+    try {
+
+        const movie = await prisma.movie.findUnique({
+            where: { id: id }
+        });
+
+        if(!movie) {
+            return res.status(404).send({ message: "Filme não encontrado." });
+        }
+
+        await prisma.movie.delete({
+            where: { id: id }
+        });
+
+    } catch (error) {
+        return res.status(500).send({ message: "Não foi possível remover o filme." });
+    }
+
+    res.status(200).send("Filme removido com sucesso");
+});
+
 app.listen(port, () => {
     console.log(`Servidor em execucao na porta ${port}`);
 });
